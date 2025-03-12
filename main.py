8f2e96d99740d48d3ee0c7b2cdd79c81af99fc32
marhansee <martinbirch@outlook.dk>
2025-03-12 20:57:40 +0100

    sample main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+def greet(name):
+    """
+    Greet the user by name.
+
+    Parameters:
+        name (str): The name of the user.
+
+    Returns:
+        str: A greeting message.
+    """
+    return f"Hello, {name}!"
+
+if __name__ == '__main__':
+    print(greet("World"))
