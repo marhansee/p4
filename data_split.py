2efc487a1892fef54ff1856c6b8cdf83be900455
marhansee <martinbirch@outlook.dk>
2025-04-25 09:50:31 +0200

    fixed year bug in script

--- data_split.py
+++ data_split.py
@@ -18,7 +18,7 @@ for filename in os.listdir(src_dir):
             date_str = filename.split("-")[1] + "-" + filename.split("-")[2] + "-" + filename.split("-")[3][:2]
             date = datetime.strptime(date_str, "%Y-%m-%d")
 
-            if date.month in [3,4,5,6,7,8,9,10,11]:  # March to November
+            if date.month in [3,4,5,6,7,8,9,10,11] and date.year == 2024:  # March to November 2024
                 shutil.move(os.path.join(src_dir, filename), os.path.join(train_dir, filename))
             else:  # December to February
                 shutil.move(os.path.join(src_dir, filename), os.path.join(test_dir, filename))
